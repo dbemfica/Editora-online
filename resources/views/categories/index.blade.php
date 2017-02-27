0e450f9bfe8044f4e7e09e796dc298932216015f
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2017-02-27 16:07:54 -0300

    Listando as categorias

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h3>Listagem de Categorias</h3>
+            <a href="{{ route('categories.create') }}" class="btn btn-primary">Nova Categoria</a>
+        </div>
+        <div class="row">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($categories as $category)
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>Açoes</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
